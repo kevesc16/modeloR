954d58a9c5d139d5de0541aec200d615372c09f0
kevesc16 <kjescoba10@gmail.com>
2022-07-06 14:14:45 +0000

    Actualizacion de tabalas

--- tablas2.sql
+++ tablas2.sql
@@ -3,7 +3,7 @@ CREATE TABLE Clientes(
 id_cliente          NUMBER (11)NOT NULL,
 Rut_Cliente         NUMBER (11)NOT NULL,
 Dv                  CHAR (1 CHAR)NOT NULL,
-Numero_pasaporte    NUMBER (50)NOT NULL,
+Numero_pasaporte    NUMBER,
 Primer_nombre       VARCHAR2 (250)NOT NULL,
 Segundo_nombre      VARCHAR2 (250),
 Apellido_paterno    VARCHAR2 (250)NOT NULL,
@@ -13,12 +13,11 @@ Genero              VARCHAR2 (250),
 Direccion           VARCHAR2 (250)NOT NULL,
 Correo_electronico  VARCHAR2 (250)NOT NULL,
 Discapacidad        VARCHAR2(250),
-Curriculum_vitae    VARCHAR2(50)NOT NULL , 
+Curriculum_vitae    VARCHAR2(50) , 
 Numero_carnet_socio NUMBER (11)NOT NULL,
 Comunas_id          NUMBER(11)NOT NULL,
 Nacionalidad_id     NUMBER(11)NOT NULL
 );
-
 CREATE TABLE Vehiculos( 
 Patente         VARCHAR2 (50)NOT NULL,
 Marca           VARCHAR2 (250)NOT NULL,
@@ -29,45 +28,39 @@ Numero_chasis   VARCHAR2 (50)NOT NULL,
 Numero_motor    VARCHAR2 (50)NOT NULL,
 Clientes_id      NUMBER(11)NOT NULL
 );
-
 CREATE TABLE Pagos ( 
-id_pago         NUMBER (11),
-pago_inicial    NUMBER (50)NOT NULL,
-pago_mensual    NUMBER (50)NOT NULL,
+id_pago         NUMBER (11)NOT NULL,
+pago_inicial    NUMBER NOT NULL,
+pago_mensual    NUMBER NOT NULL,
 Fecha_pago      DATE,
 Cheques_id      NUMBER(11)NOT NULL,
 Clientes_id     NUMBER(11)NOT NULL,
 Bancos_id       NUMBER(11)NOT NULL
 );
-
 CREATE TABLE Cheques(
 id_cheque           NUMBER (11)     NOT NULL,
-numero_cheque       NUMBER (50)     NOT NULL,
-Cantidad_a_pagar    NUMBER (50)     NOT NULL,
+numero_cheque       NUMBER NOT NULL,
+Cantidad_a_pagar    NUMBER NOT NULL,
 Bancos_id           NUMBER (11)     NOT NULL
 );
-
 CREATE TABLE Beneficios(
 id_beneficio            NUMBER (11)NOT NULL,
-Cantidad_uso            NUMBER (50)NOT NULL,
-Cantidad_maxima_uso     NUMBER(50),
+Cantidad_uso            NUMBER NOT NULL,
+Cantidad_maxima_uso     NUMBER,
 Descuento_por_tipo      VARCHAR2 (250)NOT NULL,
 Tipos_beneficios_id     NUMBER(11)NOT NULL,
 Clientes_id             NUMBER(11)NOT NULL
 );
-
 CREATE TABLE Tipos_beneficios(
 id_tipo_beneficio NUMBER (11)NOT NULL,
-Salud CHAR (1),
-Educacion CHAR (1),
-Deportes CHAR (1)
+Salud             CHAR (1),
+Educacion         CHAR (1),
+Deportes          CHAR (1)
 );
-
 CREATE TABLE Bancos(
 id_banco            NUMBER (11) NOT NULL,
 Nombre              VARCHAR2(250) NOT NULL
 );
-
 CREATE TABLE Cargas_Clientes(
 Rut_carga           NUMBER (11)NOT NULL,
 Dv                  CHAR (1 CHAR)NOT NULL,
@@ -80,27 +73,23 @@ Clientes_id         NUMBER(11)NOT NULL,
 Parentescos_id      NUMBER(11)NOT NULL,
 Comunas_id          NUMBER(11)NOT NULL
 );
-
 CREATE TABLE Telefonos(
 id_telefono         NUMBER (11) NOT NULL,
-Numero              NUMBER (50) NOT NULL,
-Tipos_numeros_id    NUMBER (50) NOT NULL,
+Numero              NUMBER  NOT NULL,
+Tipos_numeros_id    NUMBER  NOT NULL,
 Clientes_id         NUMBER (11) NOT NULL
 );
-
 CREATE TABLE Tipos_numeros(
 id_tipo_telefono    NUMBER (11) NOT NULL,
 Casa                CHAR (1),
 Oficina             CHAR (1),
 Celular             CHAR (1)
 );
-
 CREATE TABLE Comunas(
 id_comuna       NUMBER (11) NOT NULL,
 Nombre          VARCHAR2 (250) NOT NULL,
 Provincias_id   NUMBER (11) NOT NULL
 );
-
 CREATE TABLE Provincias(
 id_provincia    NUMBER (11) NOT NULL,
 Nombre          VARCHAR2 (250) NOT NULL,
@@ -110,24 +99,20 @@ CREATE TABLE Regiones(
 id_region   NUMBER (11) NOT NULL,
 Nombre      VARCHAR2 (250) NOT NULL
 );
-
 CREATE TABLE Parentescos (
 id_parentesco       NUMBER (11)NOT NULL,
 Parentesco_cliente  VARCHAR2 (250)NOT NULL,
 Pareja_id           NUMBER (11) NOT NULL
 );
-
 CREATE TABLE Parejas(
 id_pareja               NUMBER (11)NOT NULL,
 matrimonio              CHAR (1),
 acuerdo_union_civil     CHAR (1)
 );
-
 CREATE TABLE Nacionalidad(
 id_nacionalidad     NUMBER (11)    NOT NULL,
 Nombre_nacionalidad VARCHAR2 (250) NOT NULL
 );
-'PRIMARY KEY'
 ALTER TABLE Clientes ADD CONSTRAINT cliente_PK PRIMARY KEY (id_cliente);
 ALTER TABLE Vehiculos ADD CONSTRAINT vehiculo_PK PRIMARY KEY (Patente);
 ALTER TABLE Beneficios ADD CONSTRAINT beneficio_PK PRIMARY KEY(Id_beneficio);
@@ -144,8 +129,6 @@ ALTER TABLE Parejas ADD CONSTRAINT pareja_PK PRIMARY KEY (id_pareja);
 ALTER TABLE Tipos_beneficios ADD CONSTRAINT tipo_beneficio_PK PRIMARY KEY (id_tipo_beneficio);
 ALTER TABLE Telefonos ADD CONSTRAINT telefono_PK PRIMARY KEY (id_telefono);
 ALTER TABLE Tipos_numeros ADD CONSTRAINT tipo_numero_PK PRIMARY KEY (id_tipo_telefono);
-
-'Clave foranea'
     ALTER TABLE Cheques ADD CONSTRAINT Cheques_Bancos_FK FOREIGN KEY (Bancos_id)
     REFERENCES Bancos (id_banco);
     ALTER TABLE Pagos ADD CONSTRAINT Pagos_Clientes_FK   FOREIGN KEY (Clientes_id)
@@ -181,8 +164,27 @@ ALTER TABLE Tipos_numeros ADD CONSTRAINT tipo_numero_PK PRIMARY KEY (id_tipo_tel
     ALTER TABLE Parentescos ADD CONSTRAINT Parentesco_Pareja_FK FOREIGN KEY (Pareja_id)
     REFERENCES Parejas (id_pareja);
 
-INSERT INTO Bancos VALUES();
-
+INSERT INTO Bancos VALUES(1,'Banco Santander');
+INSERT INTO Nacionalidad VALUES (1, 'Chilena');
+INSERT INTO Regiones VALUES(1,'REGION METROPOLITANA');
+INSERT INTO Provincias VALUES(1,'Santiago',1);
+INSERT INTO Comunas VALUES(1,'SAN MIGUEL',1);
+INSERT INTO Clientes (
+id_cliente,
+Rut_Cliente,
+Dv,
+Primer_nombre,
+Apellido_paterno,
+Fecha_nacimiento,
+Direccion,
+Correo_electronico,
+Numero_carnet_socio,
+Comunas_id,
+Nacionalidad_id
+) VALUES (1, 21012872754, '7', 'Cristian','Ferreira', '19/04/2002','BLALLA', 'BLABLAJ',123124,1,1);
+INSERT INTO Vehiculos VALUES(1234,'HCA','FSDHG','','E','RQEWGWGRWEG','DFAHOFUF MOTOE',1 );
+INSERT INTO Cheques VALUES (1,123,1234,1);
+INSERT INTO Pagos VALUES(1,22,23,'22/05/16',1,1,1);
 
 
 
